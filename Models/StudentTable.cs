7952db3f98147c442d1ce61ed7154eed6bc4af41
Chloe Motz <finnickodair11@gmail.com>
2021-11-23 12:50:02 -0500

    Student Table Model

--- Models/StudentTable.cs
+++ Models/StudentTable.cs
@@ -2,6 +2,10 @@ namespace FinalProject_CompProg.Models
 {
     public class StudentMembers
     {
-
+        public int id {get; set;}
+        public string fullName {get; set;}
+        public int birthDate {get; set;}
+        public string collegeProgram {get; set;}
+        public string collegeYear {get;set;}
     }
 }
