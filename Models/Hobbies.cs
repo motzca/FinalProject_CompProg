0af244bae0b8d5817ca095d4787864577cb176c5
burdhasp <95309687+burdhasp@users.noreply.github.com>
2021-11-30 16:05:21 -0500

    Add files via upload

---
+++ Models/Hobbies.cs
@@ -0,0 +1,11 @@
+namespace FinalProject_CompProg.Models
+{
+    public class Hobbies
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string activityType { get; set; }
+        public string mainInterest { get; set; }
+        public int avgTimeSpent { get; set; }
+    }
+}
